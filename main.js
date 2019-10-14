501a43a77e6d7f6ef8c7475e055f364e6fd33134
Sasha Hayles <sashahayles@yahoo.com>
2019-10-13 21:24:42 -0400

    Add mousein to JS

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var mainNav = document.getElementsByClassName ('main-menu-labels');
 //console.log(myMainNavLinks);
 //function
 function toggleSub() {
+  this.nextElementSibling.classList.toggle('hide');
 
 }
 //loops
 for (i=0; i < mainNav.length; i++) {
-mainNav[i].addEventListener ('click', toggleSub);
+mainNav[i].addEventListener ('mousein', toggleSub);
 }

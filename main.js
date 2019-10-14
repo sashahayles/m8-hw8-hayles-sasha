4d3db13fa214edb17eb0030fc8dda93e362e21a4
Sasha Hayles <sashahayles@yahoo.com>
2019-10-13 21:14:24 -0400

    Create for loop and hide class

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // It is always helpful to use comments in your code!
+var mainNav = document.getElementsByClassName ('main-menu-labels');
+//console.log(myMainNavLinks);
+//function
+function toggleSub() {
+
+}
+//loops
+for (i=0; i < mainNav.length; i++) {
+mainNav[i].addEventListener ('click', toggleSub);
+}
